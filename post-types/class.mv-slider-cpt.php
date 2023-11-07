bd1ee9e5e79c193728f83ed494a6fc04e4c7545e
Aman07a <aman16.aa17@gmail.com>
2023-11-07 09:18:12 +0100

    Creating the custom post type (CPT) - part 2

--- post-types/class.mv-slider-cpt.php
+++ post-types/class.mv-slider-cpt.php
@@ -5,11 +5,33 @@ if (!class_exists('MV_Slider_Post_Type')) {
     {
         function __construct()
         {
-            add_action('init', array($this,  'create_post_type'));
+            add_action('init', [$this, 'create_post_type']);
         }
 
         public function create_post_type()
         {
+            register_post_type('mv-slider', [
+                'label' => 'Slider',
+                'description' => 'Sliders',
+                'labels' => [
+                    'name' => 'Sliders',
+                    'singular_name' => 'Slider',
+                ],
+                'public' => true,
+                'supports' => ['title', 'editor', 'thumbnail'],
+                'hierarchical' => false,
+                'show_ui' => true,
+                'show_in_menu' => true,
+                'menu_position' => 5,
+                'show_in_admin_bar' => true,
+                'show_in_nav_menus' => true,
+                'can_export' => true,
+                'has_archive' => false,
+                'exclude_from_search' => false,
+                'publicly_queryable' => true,
+                'show_in_rest' => true,
+                'menu_icon' => 'dashicons-images-alt2',
+            ]);
         }
     }
 }
